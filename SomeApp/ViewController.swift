e9140d99ddb54a41e5f7483a14674f0394e3bc88
bemadeveloper <akbermet1990@icloud.com>
2024-02-01 16:48:28 +0600

    Initial Commit

---
+++ SomeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeApp
+//
+//  Created by Bema on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
